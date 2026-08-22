3122bd2a8fbd900ab3e955091b8fcf3a136c7f38
faqihrayhan <fqhrayhan19@gmail.com>
2026-08-23 06:48:08 +0700

    revert: remove BeamsBackground completely and restore clean dark glass theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,10 @@ import { ExperienceSection } from "@/components/sections/ExperienceSection";
 import { ProjectSection } from "@/components/sections/ProjectSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 import { Footer } from "@/components/sections/Footer";
-import { BeamsBackground } from "@/components/ui/BeamsBackground";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen bg-[#0c0d0e] relative selection:bg-orange-500/30 selection:text-orange-200">
-      {/* Animated Beams Background */}
-      <BeamsBackground intensity="medium" />
-
-      {/* Main Content Sections */}
       <div className="relative z-10 flex flex-col min-h-screen">
         <Navbar />
         <Hero />
